aa7204867da7d9d2b625babf8a77275422646532
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2022-11-03 15:05:08 +0300

    first commit

---
+++ project 1/app.js
@@ -0,0 +1,3 @@
+// Template literals (template strings) allow you to use strings or embedded expressions in the form of a string. They are enclosed in backticks `` 
+const name = `jack`;
+console.log(`Hello ${name}!`); // Hello Jack!
